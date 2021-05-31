14bcf804ff591f32395a1655463fcbbdff8b7491
josuec4321 <josuec4321@gmail.com>
2021-05-30 20:55:45 -0500

    servcio para obtener citas con acto medico ydatos del paciente y maeo del acto medico

--- SISFAHD/DTOs/CitaDTO.cs
+++ SISFAHD/DTOs/CitaDTO.cs
@@ -4,7 +4,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Linq;
 using System.Threading.Tasks;
-
+using SISFAHD.Entities;
 
 namespace SISFAHD.DTOs
 {
@@ -57,5 +57,26 @@ namespace SISFAHD.DTOs
     public class ContenidoDatosMedicoCita
     {
         public string nombre_apellido_medico { get; set; }
+    }
+    // CITADTO -> Correctamente Mapeado
+    public class CitaDTO2
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string estado_atencion { get; set; }
+        public string estado_pago { get; set; }
+        public DateTime? fecha_cita { get; set; }
+        public DateTime? fecha_pago { get; set; }
+        public DateTime? fecha_reserva { get; set; }
+        public Paciente datos_paciente { get; set; }
+        public string enlace_cita { get; set; }
+        public double precio_neto { get; set; }
+        public double calificacion { get; set; }
+        public List<String> observaciones { get; set; } = new List<string>();
+        public string tipo_pago { get; set; }
+        public string id_turno { get; set; }
+        public ActoMedico datos_acto_medico { get; set; }
+        public DateTime? fecha_cita_fin { get; set; }
     }
 }
