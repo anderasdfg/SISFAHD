be65e95b6c64c3dc23afb1a93ec5e13fc1f37e99
VivianaPereaA <vpereapaza@gmail.com>
2021-06-10 00:54:53 -0500

    cambios usuarioservice

---
+++ SISFAHD/Controllers/MiUsuarioController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SISFAHD.DTOs;
+using SISFAHD.Entities;
+using SISFAHD.Helpers;
+using SISFAHD.Services;
+using System.Web.Http.Cors;
+
+namespace SISFAHD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MiUsuarioController:ControllerBase
+    {
+        private readonly UsuarioService _usuarioservice;
+        public MiUsuarioController(UsuarioService usuarioservice)
+        {
+            _usuarioservice = usuarioservice;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<List<UsuarioDTO>>> GetAllUsuarios()
+        {
+            return await _usuarioservice.GetAllUsuarios();
+        }
+
+        [HttpPost("Registrar")]
+        public ActionResult<Usuario> CreateUsuario(Usuario usuario)
+        {
+            Usuario objetousuario = _usuarioservice.CreateUsuario(usuario);
+            return objetousuario;
+        }
+
+        [HttpPut("Modificar")]
+        public ActionResult<Usuario> ModificarUsuario([FromQuery] Usuario id)
+        {
+            Usuario usuario = _usuarioservice.ModificarUsuario(id);
+            return usuario;
+        }
+    }
+}
--- SISFAHD/Services/UsuarioService.cs
+++ SISFAHD/Services/UsuarioService.cs
@@ -36,41 +36,108 @@ namespace SISFAHD.Services
             usuario = _usuarios.Find(usuario => usuario.id == id).FirstOrDefault();
             return usuario;
         }
+
+        public Usuario CreateUsuario(Usuario usuario)
+        {
+            _usuarios.InsertOne(usuario);
+            return usuario;
+        }
+
+        public Usuario ModificarUsuario(Usuario usuario)
+        {
+
+            var filter = Builders<Usuario>.Filter.Eq("id", usuario.id);
+            var update = Builders<Usuario>.Update
+                .Set("nombre", usuario.datos.nombre)
+                .Set("apellido paterno", usuario.datos.apellido_Paterno)
+                .Set("apellido materno", usuario.datos.apellido_Materno)
+                .Set("tipo de documento", usuario.datos.tipo_Documento)
+                .Set("numero de documento", usuario.datos.numero_Documento)
+                .Set("fecha de nacimiento", usuario.datos.fecha_nacimiento)
+                .Set("nro de telefono", usuario.datos.telefono)
+                .Set("sexo", usuario.datos.sexo);
+
+            usuario = _usuarios.FindOneAndUpdate<Usuario>(filter, update, new FindOneAndUpdateOptions<Usuario>
+            {
+                ReturnDocument = ReturnDocument.After
+            });
+            return usuario;
+        }
+
         public async Task<List<UsuarioDTO>> GetAllUsuarios()
         {
             List<UsuarioDTO> gusuario = new List<UsuarioDTO>();
-            var añadir = new BsonDocument("$addFields",
-   new BsonDocument("id_rol",
-   new BsonDocument("$toObjectId", "$rol")));
-            var ver = new BsonDocument("$lookup",
-            new BsonDocument
-                {
+           
+            var addFields = new BsonDocument("$addFields",
+     new BsonDocument("id_rol",
+     new BsonDocument("$toObjectId", "$rol")));
+            var lookup = new BsonDocument("$lookup",
+             new BsonDocument
+                 {
             { "from", "roles" },
             { "localField", "id_rol" },
             { "foreignField", "_id" },
             { "as", "urol" }
-                });
+                 });
             var unwind = new BsonDocument("$unwind",
-            new BsonDocument
-                {
+             new BsonDocument
+                 {
             { "path", "$urol" },
             { "preserveNullAndEmptyArrays", true }
-                });
+                 });
+            var addFields2 = new BsonDocument("$addFields",
+              new BsonDocument("datos",
+              new BsonDocument("apellidos",
+              new BsonDocument("$concat",
+              new BsonArray
+                              {
+                        "$datos.apellido_paterno",
+                        " ",
+                        "$datos.apellido_materno"
+                              }))));
             var project = new BsonDocument("$project",
-            new BsonDocument
-                {
+             new BsonDocument
+                 {
             { "_id", 1 },
             { "datos", 1 },
             { "estado", 1 },
             { "urol",
     new BsonDocument("nombre", 1) }
-                });
+                 });
+            var project2 = new BsonDocument("$project",
+             new BsonDocument("datos",
+             new BsonDocument
+                     {
+                { "apellido_paterno", 0 },
+                { "apellido_materno", 0 }
+                     }));
+            var addFields3 = new BsonDocument("$addFields",
+             new BsonDocument("datos",
+             new BsonDocument("nombresyapellidos",
+             new BsonDocument("$concat",
+             new BsonArray
+                             {
+                        "$datos.nombre",
+                        " ",
+                        "$datos.apellidos"
+                             }))));
+            var project3 = new BsonDocument("$project",
+            new BsonDocument("datos",
+            new BsonDocument
+                    {
+                { "nombre", 0 },
+                { "apellidos", 0 }
+                    }));
 
             gusuario = await _usuarios.Aggregate()
-                .AppendStage<dynamic>(añadir)
-                .AppendStage<dynamic>(ver)
+                .AppendStage<dynamic>(addFields)
+                .AppendStage<dynamic>(lookup)
                 .AppendStage<dynamic>(unwind)
-                .AppendStage<UsuarioDTO>(project)
+                .AppendStage<dynamic>(addFields2)
+                .AppendStage<dynamic>(project)
+                .AppendStage<dynamic>(project2)
+                .AppendStage<dynamic>(addFields3)
+                .AppendStage<UsuarioDTO>(project3)
                 .ToListAsync();
             return gusuario;
         }
