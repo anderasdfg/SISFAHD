373a9673702d74ebb3e0052bc868f8e2b8b4cee9
sebasmm12 <smirandamanrique@gmail.com>
2021-06-20 03:25:37 -0500

    Subida de controllador para el futuro CUS CHAT

---
+++ SISFAHD/Controllers/MensajeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISFAHD.Controllers
+{
+    public class MensajeController : ControllerBase
+    {
+        public IActionResult SendMessage()
+        {
+            return Ok();
+        }
+    }
+}
