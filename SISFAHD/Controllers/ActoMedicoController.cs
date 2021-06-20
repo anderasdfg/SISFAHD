47b0e74d6189b8c4292f7189ea87bd7ceecdba18
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2021-06-20 01:11:37 -0500

    Creacion de api para Acto medico ARA ARA

---
+++ SISFAHD/Controllers/ActoMedicoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SISFAHD.Entities;
+using SISFAHD.Services;
+
+namespace SISFAHD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActoMedicoController : ControllerBase
+    {
+        private readonly ActoMedicoService _actomedicoservice;
+        public ActoMedicoController(ActoMedicoService actomedicoservice)
+        {
+            _actomedicoservice = actomedicoservice;
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<ActoMedico>> GetActoMedicoById([FromQuery] string id)
+        {
+            return await _actomedicoservice.GetById(id);
+        }
+
+        [HttpPost("Registrar")]
+        public async Task<ActionResult<ActoMedico>> PostActoMedico(ActoMedico actomedico)
+        {
+            return await _actomedicoservice.CrearActoMedico(actomedico);
+        }
+        [HttpPut("Actualizar")]
+        public async Task<ActionResult<ActoMedico>> PutActoMedico(ActoMedico actomedico)
+        {
+            return await _actomedicoservice.ModificarActoMedico(actomedico);
+        }
+    }
+}
--- SISFAHD/Services/ActoMedicoService.cs
+++ SISFAHD/Services/ActoMedicoService.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using SISFAHD.Entities;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,33 @@ namespace SISFAHD.Services
         public async Task<List<ActoMedico>> GetAll()
         {
            return await _actoMedico.Find(x => true).ToListAsync();
+        }
+
+        public async Task<ActoMedico> GetById(string id)
+        {
+            return await _actoMedico.Find(x => x.id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<ActoMedico> CrearActoMedico(ActoMedico actomedico)
+        {
+            actomedico.fecha_creacion = new DateTime();
+            actomedico.fecha_atencion = new DateTime();
+            await _actoMedico.InsertOneAsync(actomedico);
+            return actomedico;
+        }
+
+        public async Task<ActoMedico> ModificarActoMedico(ActoMedico actomedico)
+        {
+            var filter = Builders<ActoMedico>.Filter.Eq("id", ObjectId.Parse(actomedico.id));
+            var update = Builders<ActoMedico>.Update
+                .Set("medicacion", actomedico.medicacion)
+                .Set("diagnostico", actomedico.diagnostico)
+                .Set("signos_vitales", actomedico.signos_vitales)
+                .Set("anamnesis", actomedico.anamnesis)
+                .Set("indicaciones", actomedico.indicaciones);
+
+            await _actoMedico.UpdateOneAsync(filter, update);
+            return actomedico;
         }
     }
 }
