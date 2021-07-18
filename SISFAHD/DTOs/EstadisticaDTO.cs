b133d2cf64c5e79777cfb86a1a1941618d3e3440
RenzoGuerra <renzoguerra041100@gmail.com>
2021-07-18 05:21:29 -0500

    Estadisticas Cambios en info de  Apis

--- SISFAHD/DTOs/EstadisticaDTO.cs
+++ SISFAHD/DTOs/EstadisticaDTO.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Attributes;
+using SISFAHD.DTOs;
+using SISFAHD.Entities;
+using SISFAHD.Services;
 
 namespace SISFAHD.DTOs
 {
@@ -36,5 +42,59 @@ namespace SISFAHD.DTOs
     {
         public string _id { get; set; }
         public Int32 cantidad { get; set; }
+    }
+    public class CitasxMedicos
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public List<Turnos> turnos { get; set; }
+        public Suscripcion suscripcion { get; set; }
+        public Datos_basicos datos_basicos { get; set; }
+        public string id_especialidad { get; set; }
+        public List<Cita> citas { get; set; }
+        public int cantidad { get; set; }
+    }
+    public class CitasxMedicosyEstadoAtencion
+    {
+        public int cantidad { get; set; }
+        public Medico datos_medico { get; set; }
+        public string estado_atencion { get; set; }
+    }
+    public class CitasxEspecialidad
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public int cantidad { get; set; }
+        public Especialidad datos_especialidad { get; set; }
+    }
+    public class CitasxEspecialidadyEstadoAtencion
+    {
+        public int cantidad { get; set; }
+        public Especialidad datos_especialidad { get; set; }
+        public string estado_atencion { get; set; }
+    }
+    public class CitasxEstadoAtencion
+    {
+        public int cantidad { get; set; }
+        public string estado_atencion { get; set; }
+    }
+    public class CitasxPaciente
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public Datos_Paciente datos { get; set; }
+        public Usuario datos_paciente { get; set; }
+        public List<Cita> datos_citas { get; set; }
+        public int cantidad { get; set; }
+    }
+    public class CitasxPacienteyEstadoAtencion
+    {
+        public int cantidad { get; set; }
+        public string estado_cita { get; set; }
+        public Usuario datos_usuario { get; set; }      
+        public string id_usuario { get; set; }
     }
 }
