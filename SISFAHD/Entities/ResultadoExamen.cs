f5fe09f9e5944774a817d29869ec252852d6fa6a
MirkoRZ <mirkorojas345@gmail.com>
2021-10-17 01:40:53 -0500

    Mapeo de la entidad ResultadoExamen

---
+++ SISFAHD/Entities/ResultadoExamen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Attributes;
+using SISFAHD.DTOs;
+using SISFAHD.Services;
+
+namespace SISFAHD.Entities
+{
+    public class ResultadoExamen
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement("nombre")]
+        public string nombre { get; set; }
+        [BsonElement("tipo")]
+        public string tipo { get; set; }
+
+        [BsonElement("observaciones")]
+        public string observaciones { get; set; }
+
+        [BsonElement("documento_anexo")]
+        public List<string> documento_anexo { get; set; }
+
+        [BsonElement("codigo")]
+        public string codigo { get; set; }
+    }
+}
