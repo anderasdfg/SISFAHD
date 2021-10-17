05fb36843b69f544cf391ad7a8917a82e360c7a2
MirkoRZ <mirkorojas345@gmail.com>
2021-10-17 03:03:57 -0500

    Mapeo de entidad PacienteDTO, creación de ResultadoExamenService y ResultadoExamenController

---
+++ SISFAHD/Controllers/ResultadoExamenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using SISFAHD.DTOs;
+using SISFAHD.Entities;
+using SISFAHD.Helpers;
+using SISFAHD.Services;
+using System.Web.Http.Cors;
+
+namespace SISFAHD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResultadoExamenController : ControllerBase
+    {
+        private readonly ResultadoExamenService _resultadoExamenService;
+        public ResultadoExamenController(ResultadoExamenService resultadoExamenService)
+        {
+            _resultadoExamenService = resultadoExamenService;
+        }
+        [HttpGet]
+        public ActionResult<List<PacienteDTO>> getExamenesSolicitados([FromQuery] Paciente p)
+        {
+            return _resultadoExamenService.getExamenesSolicitados(p);
+        }
+    }
+}
