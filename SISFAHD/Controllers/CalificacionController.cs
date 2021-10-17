d8d493ce89b333a4d2a95dcfcbbfcdabf4fa3269
Anghelo Flores <anghelofloresfano@gmail.com>
2021-10-17 01:20:16 -0500

    Ya no sé lo que hago xd

---
+++ SISFAHD/Controllers/CalificacionController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SISFAHD.Services;
+using SISFAHD.Entities;
+using SISFAHD.Helpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SISFAHD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionController : ControllerBase
+    {
+
+        private readonly CalificacionService  _atencionservices;
+        private readonly CitaService _cita;
+        private readonly IFileStorage _fileStorage;
+
+        public CalificacionController(CalificacionService atencionservices, IFileStorage fileStorage) {
+
+            _atencionservices = atencionservices;
+            _fileStorage = fileStorage;
+        }
+
+        [HttpGet("all")]
+
+        public ActionResult<List<Opiniones>> GetAll() {
+
+            return _atencionservices.GetaAll();
+        }
+
+        [HttpGet("pruebaEstado")]
+
+        public int PruebaEstado(string id)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(id))
+                {
+                    return _atencionservices.GetEstadobyIdCita(id);
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        [HttpGet("Registrar")]
+        public async Task<ActionResult<Opiniones>> Calificar(Opiniones opiniones)
+        {
+            return await _atencionservices.CrearOpiniones(opiniones);
+        }
+
+      
+
+
+    }
+}
---
+++ SISFAHD/Entities/Opiniones.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Attributes;
+using SISFAHD.DTOs;
+using SISFAHD.Services;
+
+namespace SISFAHD.Entities
+{
+    public class Opiniones
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("datos_medico")]
+        public Dato_medico datos_medico { get; set; }
+
+        [BsonElement("datos_paciente")]
+        public Datos_paciente datos_paciente { get; set; }
+
+        [BsonElement("calificacion")]
+        public int calificacion { get; set; }
+
+        [BsonElement("observacion")]
+        public string observacion { get; set; }
+
+        [BsonElement("datos_cita")]
+        public Datos_cita datos_cita { get; set; }
+
+
+        public class Dato_medico { 
+            public string id_medico { get; set; }
+            public string nombre { get; set; }
+
+        }
+        public class Datos_paciente
+        {
+            public string id_paciente { get; set; }
+            public string nombre { get; set; }
+            public string apellido { get; set; }
+        }
+        public class Datos_cita
+        {
+            public string fecha { get; set; }
+            public string id_cita { get; set; }
+        }
+    }
+}
