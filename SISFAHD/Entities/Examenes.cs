15570ce5408e35bc1ebe9df62549ae7cf07e6838
Stefano GL <steph.gl@hotmail.com>
2021-11-07 01:12:44 -0500

    modificacion modelo examenes

--- SISFAHD/Entities/Examenes.cs
+++ SISFAHD/Entities/Examenes.cs
@@ -20,7 +20,16 @@ namespace SISFAHD.Entities
         public string descripcion { get; set; }
         [BsonElement("precio")]
         public double precio { get; set; }
+        [BsonElement("id_especialidad")]
+        public string idEspecialidad { get; set; }
+        [BsonElement("duracion")]
+        public string duracion { get; set; }
+        [BsonElement("recomendaciones_previas")]
+        public string recomendacionesPrevias { get; set; }
+        [BsonElement("recomendaciones_posteriores")]
+        public string recomendacionesPosteriores { get; set; }
 
-        
+
+
     }
 }
